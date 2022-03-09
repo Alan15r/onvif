4c08637b2ef24c188fc9a16ebfee0ab9160c791a
alan <alan.tyloev@ptolemey.net>
2022-03-09 10:24:20 +0300

    first commit

---
+++ kod.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/use-go/onvif"
+)
+
+func main() {
+	//создаем девайс(камеру) и подключаемся к ней
+	device, _ := onvif.NewDevice(onvif.DeviceParams{Xaddr: "172.22.226.8:10080"})
+
+	//ptz
+	//device
+	//analytics
+	//imaging
+	//media
+
+	//делаем запрос по нужному эндпоинту
+	resp, err := device.CallMethod("media")
+	if err != nil {
+		log.Print("ERROR: ", err.Error())
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("ERROR2: ", err.Error())
+	}
+
+	fmt.Println(string(body))
+}
